0c9a89403a79f4739f47db4f03e34c2c505440c1
sonnyyu <sonnyyuirm@gmail.com>
2015-04-23 21:55:41 -0400

    add remove.sh

---
+++ remove.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+./scripts/feeds uninstall collectd-mod-postgresql bird4 phidget21 phidget-service \
+hsflowd xz memtester kmod-gpiotoggling kmod-siit kmod-loop-aes \
+bwping fprobe-ulog  mutella libleptonica libvncserver reaver streamripper yafc \
+kmod-wprobe krb5-libs krb5-server krb5-client libmikmod  openswan \
+ettercap  osirisd miredo-client  miredo-common miredo-server gadgetfs-test-bulk \
+gadgetfs-test-isoc snort-wireless sslcat tor aria2 ddns-scripts  \
+python-cwiid  pmacctd  pmacct-client  baresip \
+php5-pecl-dio php5-pecl-apc  php5-pecl-http python-yapsnmp \
+tor-alpha  forked-daapd minidlna dmapd libdlna \
+ushare hplip gst-python tesseract  cups hidapi  xmpp4r cups-bjnp \
+speedtouch-usb-firmware snort snort-mysql snort-pgsql gdb usbip \
+python-egenix-mx gst-ffmpeg python-psycopg bitchx gdb libgupnp-dlna rygel  libiksemel asterisk18-codec-g729 nufw
